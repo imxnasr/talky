03bad3a33150446987eb91c76e51d1efe6c976ff
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-07-18 19:18:00 +0300

    :bug: Fixed Providers issue

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -8,18 +8,19 @@ interface ProvidersProps {
 
 export const Providers: FC<ProvidersProps> = ({ children }) => {
   useEffect(() => {
-    // Check if the user have selected a theme
-    const selectedTheme = localStorage.getItem("theme");
-    if (selectedTheme) {
-      // If the user had selected a theme, use that
-      document.documentElement.classList.add(selectedTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)")) {
-      // Use the dark theme if the system theme is dark
-      document.documentElement.classList.add("dark");
-    } else {
-      // Use the light theme if the system theme is light
-      document.documentElement.classList.add("dark");
+    if (typeof window !== undefined) {
+      // Check if the user have selected a theme
+      const selectedTheme = localStorage.getItem("theme");
+      if (selectedTheme) {
+        // If the user had selected a theme, use that
+        document.documentElement.classList.add(selectedTheme);
+      } else {
+        // Use the dark theme if the system theme is dark
+        document.documentElement.classList.add("dark");
+        // Set the dark theme as the default
+        localStorage.setItem("theme", "dark");
+      }
     }
-  }, [localStorage]);
-  return <div>{children}</div>;
+  }, []);
+  return children;
 };
