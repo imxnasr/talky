e0131eb6946127b40cf212cab7d3d024db3dbc96
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-16 21:45:24 +0300

    🚧 Test action function

--- app/utils/functions.ts
+++ app/utils/functions.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10; // Number of salt rounds for bcrypt
