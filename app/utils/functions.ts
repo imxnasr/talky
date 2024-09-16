2a8051d1799d39a0cee776e54384b2f114a143b1
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-16 18:09:05 +0300

    ✨ Created hashing and comparing password fucntions

--- app/utils/functions.ts
+++ app/utils/functions.ts
@@ -1,5 +1,20 @@
-import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-export const sha1 = (data: string) => {
-  return crypto.createHash("sha1").update(data, "binary").digest("hex");
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 10; // Number of salt rounds for bcrypt
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error("Error hashing password");
+  }
+};
+
+export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+  try {
+    const match = await bcrypt.compare(password, hashedPassword);
+    return match; // Returns true if passwords match, false otherwise
+  } catch (error) {
+    throw new Error("Error comparing passwords");
+  }
 };
