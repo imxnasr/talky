b724c60f303b100972aecd01882278c36074fc9d
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-18 00:33:41 +0300

    ✨ Protected chats routes

--- app/routes/(chatsLayout).tsx
+++ app/routes/(chatsLayout).tsx
@@ -1,5 +1,13 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import { Navbar } from "~/components";
+import { getSession } from "~/sessions";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (!session.has("id")) return redirect("/login");
+  return null;
+};
 
 export default () => {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,8 @@
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { getSession } from "~/sessions";
 
-export const loader = ({ request }: LoaderFunctionArgs) => {
-  return redirect("/login");
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (session.has("id")) return redirect("/chats");
+  else return redirect("/login");
 };
