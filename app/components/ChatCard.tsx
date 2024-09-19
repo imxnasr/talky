a3d709ce9ed3e58074dedafa8b431ba3c3eb8969
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-20 01:13:42 +0300

    :recycle: Fetched chats in chats page

--- app/components/ChatCard.tsx
+++ app/components/ChatCard.tsx
@@ -2,23 +2,29 @@ import { FC } from "react";
 import { Avatar } from "~/components";
 
 interface ChatCardProps {
+  name: string;
+  message: {
+    body: string;
+    image: string;
+    createdAt: string;
+  };
   selected?: boolean;
 }
 
-export const ChatCard: FC<ChatCardProps> = ({ selected = false }) => {
+export const ChatCard: FC<ChatCardProps> = ({ name, message, selected = false }) => {
   return (
     <div className={`w-full my-2 flex cursor-pointer p-3 rounded-2xl gap-2 ${selected && "bg-slateSecondary"}`}>
       {/* Avatar */}
       <Avatar />
       {/* Texts */}
       <div>
-        <h4 className="mt-1">John Smith</h4>
-        <p className="text-colorSecondary">How are you today?</p>
+        <h4 className="mt-1">{name}</h4>
+        <p className="text-colorSecondary">{message.body}</p>
       </div>
       {/* Info */}
       <div className="flex flex-col">
         {/* Time */}
-        <p className="text-colorSecondary text-sm">4m</p>
+        <p className="text-colorSecondary text-sm">{message.createdAt}</p>
       </div>
     </div>
   );
--- app/routes/(chatsLayout).chats.tsx
+++ app/routes/(chatsLayout).chats.tsx
@@ -7,23 +7,52 @@ import prisma from "~/utils/prisma";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
   const user = await prisma.user.findUnique({
-    where: { username: session.get("username") },
-    include: { chats: true },
+    where: { id: session.get("id") },
+    select: {
+      // Get all chats the user is part of
+      chats: {
+        include: {
+          users: {
+            // Get all users in the chat except the current user
+            where: {
+              id: { not: session.get("id") },
+            },
+            select: {
+              id: true,
+              username: true,
+              name: true,
+              avatar: true,
+            },
+          },
+          // Get the last message in each chat
+          messages: {
+            select: {
+              body: true,
+              image: true,
+              createdAt: true,
+            },
+            orderBy: {
+              createdAt: "desc",
+            },
+            take: 1,
+          },
+        },
+      },
+    },
   });
-  console.log(user?.chats);
-  return user?.chats;
+  const chats = user?.chats;
+  return { chats };
 };
 
 export default () => {
   const data = useLoaderData<typeof loader>();
+  const chats = data?.chats;
   return (
     <main className="flex flex-1">
       {/* Chats Section */}
       <section className="flex flex-col min-w-64 mr-10 overflow-auto">
         <Search />
-        {data.map((chat: any) => (
-          <ChatCard key={chat.id} {...chat} />
-        ))}
+        {chats?.length ? chats?.map((chat: any, index: number) => <ChatCard key={index} name={chat.isGroup ? chat.name : chat.users[0].name} message={chat.messages[0]} />) : <p className="text-colorSecondary text-center mt-4">No chats yet</p>}
       </section>
       {/* Messaging Section */}
       <Chat />
