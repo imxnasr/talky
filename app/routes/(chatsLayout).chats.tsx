b99b25ae44143b8a9264246ea6555b102c3936f6
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-19 23:33:26 +0300

    :recycle: Fetched chats in chats page

--- app/routes/(chatsLayout).chats.tsx
+++ app/routes/(chatsLayout).chats.tsx
@@ -1,17 +1,26 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { Chat, ChatCard, Search } from "~/components";
+import { getSession } from "~/sessions";
+import prisma from "~/utils/prisma";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  const user = await prisma.user.findUnique({
+    where: { username: session.get("username") },
+    include: { chats: true },
+  });
+  console.log(user?.chats);
+  return user?.chats;
+};
 
 export default () => {
-  const data = [{}];
+  const data = useLoaderData<typeof loader>();
   return (
     <main className="flex flex-1">
       {/* Chats Section */}
       <section className="flex flex-col min-w-64 mr-10 overflow-auto">
         <Search />
-        <ChatCard />
-        <ChatCard selected />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
         {data.map((chat: any) => (
           <ChatCard key={chat.id} {...chat} />
         ))}
