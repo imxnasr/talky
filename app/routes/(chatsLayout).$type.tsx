a0dd550b112baaeafe9ce7d659f351edf7b6b6b5
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-09-26 19:46:40 +0300

    :recycle: Refactor chats type page with useFetch

--- app/routes/(chatsLayout).$type.tsx
+++ app/routes/(chatsLayout).$type.tsx
@@ -1,14 +1,12 @@
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useOutlet, useParams } from "@remix-run/react";
-import { useEffect, useState } from "react";
 import { ChatCard, ChatCardLoader, Search } from "~/components";
+import { useFetch } from "~/hooks";
 import { getSession } from "~/sessions";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const allowedTypes = ["chats", "groups"];
-  if (!allowedTypes.includes(params.type as string)) {
-    return redirect("/chats");
-  }
+  if (!allowedTypes.includes(params.type as string)) return redirect("/chats");
   const session = await getSession(request.headers.get("Cookie"));
   return session.get("id");
 };
@@ -17,29 +15,18 @@ export default () => {
   const userId = useLoaderData<typeof loader>();
   const outlet = useOutlet();
   const params = useParams();
-  const [isLoading, setIsLoading] = useState(true);
-  const [chats, setChats] = useState([]);
-
-  const getChats = async () => {
-    setIsLoading(true);
-    const res = await fetch("/api/chats?type=" + params.type + "&id=" + userId);
-    const data = await res.json();
-    setChats(data.chats);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getChats();
-  }, [params.type]);
+  const { data, error, loading } = useFetch("/api/chats?type=" + params.type + "&id=" + userId);
 
   return (
     <main className="flex flex-1">
       {/* Chats Section */}
       <section className="flex flex-col w-80 mr-10 overflow-auto">
         <Search />
-        {isLoading ? (
+        {/* TODO: Add error handling */}
+        {loading ? (
           <ChatCardLoader />
-        ) : chats.length > 0 ? (
-          chats.map((chat: any, index: number) => (
+        ) : data?.chats.length > 0 ? (
+          data?.chats.map((chat: any, index: number) => (
             <Link to={`/${params.type}/${chat.id}`}>
               <ChatCard key={index} name={chat.isGroup ? (chat.name as string) : chat.users[0].name} message={chat.messages[0]} selected={params.id === chat.id} />
             </Link>
