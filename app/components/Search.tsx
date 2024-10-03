5a5edde5065863d45b2721cbf3a77b314fc76b24
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-10-03 18:15:27 +0300

    🐛 Fixed search input width

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,18 +1,22 @@
+import { useNavigate } from "@remix-run/react";
 import { FC, FormEvent, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 interface SearchProps {}
 
 export const Search: FC<SearchProps> = () => {
+  const navigate = useNavigate();
   const [search, setSearch] = useState("");
   const handleSearch = (e: FormEvent) => {
     e.preventDefault();
     console.log("SEARCH:", search);
+    if (search.length === 0) return;
+    return navigate(`/search?q=${search}`);
   };
   return (
-    <form className="bg-slateSecondary flex items-center py-2 px-4 rounded-xl text-colorSecondary h-12" onSubmit={handleSearch}>
+    <form className="bg-slateSecondary flex items-center px-4 rounded-xl text-colorSecondary h-12" onSubmit={handleSearch}>
       <FiSearch className="size-6 mr-2" />
-      <input className="bg-transparent w-36 h-full" type="text" name="search" placeholder="Search" defaultValue={search} onChange={(e) => setSearch(e.target.value)} />
+      <input className="bg-transparent w-full h-full " type="text" name="search" placeholder="Search" defaultValue={search} onChange={(e) => setSearch(e.target.value)} />
     </form>
   );
 };
