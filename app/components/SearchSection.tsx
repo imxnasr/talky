1894e6e91d4f8532ac7858845f315991053046ca
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-10-15 23:05:58 +0300

    ✨ Enhance the UI of chats in search section

--- app/components/SearchSection.tsx
+++ app/components/SearchSection.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
-import { Input } from "./";
+import { Avatar, Input } from "./";
+import { Link } from "@remix-run/react";
 
 interface SearchSectionProps {
   loading: boolean;
@@ -11,16 +12,18 @@ interface SearchSectionProps {
 export const SearchSection: FC<SearchSectionProps> = ({ loading, data, q, setQ }) => {
   return (
     <div className="flex-1">
-      <Input type="text" name="search" id="search" placeholder="Search..." value={q} onChange={(e) => setQ(e.target.value)} noLabel />
+      <Input type="text" name="search" id="search" placeholder="Search..." value={q} onChange={(e) => setQ(e.target.value)} noLabel className="mb-4" />
       {loading ? (
         <div className="text-center">Loading...</div>
       ) : data?.users?.length > 0 ? (
         <div className="flex flex-col gap-4">
           {data?.users.map((user: any, index: number) => (
-            <div key={index} className="flex gap-4 items-center">
-              <img className="w-12 h-12 rounded-full" src={user.avatar || "avatar.jpg"} alt={user.name} />
-              <p>{user.name}</p>
-            </div>
+            <Link to={`some-chat-${index + 1}`} key={index}>
+              <div className="flex gap-4 items-center">
+                <Avatar twSize={14} />
+                <p>{user.name}</p>
+              </div>
+            </Link>
           ))}
         </div>
       ) : (
